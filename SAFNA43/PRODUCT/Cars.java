02ffd1eaa394c0ad7167476c9ce9feffc951c97c
safnashan <103172193+safnashan@users.noreply.github.com>
2022-04-07 12:30:50 +0530

    Add files via upload

---
+++ SAFNA43/PRODUCT/Cars.java
@@ -0,0 +1,27 @@
+package product;
+
+public class Cars {
+
+	public static void main(String[] args) {
+        Product p1 = new Product();
+        p1.pcode  = "Cars123";
+        p1.pname = "Benz";
+        p1.price = 3500000;
+        System.out.println("Displaying p1: ");
+        p1.display();
+        
+       Product p2 = new Product("jaguar","Cars426",500000);
+       System.out.println("Displaying p2:  ");
+       p2.display();
+       
+       Product p3 = new Product("maruti","Cars800",300000);
+       System.out.println("Displaying p3:  ");
+       p3.display();
+       Product p = p3.getPrice()<(p1.price<p2.price?p1.price:p2.price)?p3:(p1.price<p2.price?p1:p2);
+       System.out.println("\nDisplaying product with lowest price:" );
+       p.display();
+	}
+	
+	                                     
+
+}
