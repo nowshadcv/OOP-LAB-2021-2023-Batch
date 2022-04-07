bba1d89605f28b09c4d2a8320ff75c043709ab3b
SnehaPM05 <100551628+SnehaPM05@users.noreply.github.com>
2022-04-07 12:24:37 +0530

    Add files via upload

---
+++ 50- SNEHA PM/2-MATRIX ADDITION/Matrix.java
@@ -0,0 +1,49 @@
+package matrix;
+import java.util.Scanner;
+
+public class Matrix {
+
+	public static void main(String[] args) {
+		int p,q,m,n;
+		Scanner s = new Scanner (System.in) ;
+		System.out.print ("Enter number of rows in Matrix A:");
+		p = s.nextInt();
+		System.out.print ("Enter number of columns in matrix A:");
+		q = s.nextInt();
+		System.out.print ("Enter number of rows in Matrix B:");
+		m = s.nextInt();
+		System.out.print ("Enter number of columns in matrix B:");
+		n = s.nextInt();
+		 
+		if( p == m && q == n ){
+			int a[][] = new int[p][q] ;
+			int b[][] = new int[m][n] ;
+			System.out.println("Enter the elements of matrix A:");
+			for ( int i = 0 ; i < p ; i++)
+				for (int j = 0 ; j < q ; j++)
+					a[i][j] = s.nextInt();
+			System.out.println("Enter the elements of matrix B:");
+			for ( int i = 0 ; i < m ; i++)
+				for (int j = 0 ; j < n ; j++)
+					b[i][j] = s.nextInt();
+			System.out.println("Matrix A:") ;
+			for ( int i =0 ; i < p ; i++) {
+				for ( int j = 0 ; j < q ; j++)
+					System.out.print(a[i][j] + "  " ) ;
+				System.out.println() ;}
+			System.out.println("Matrix B:") ;
+			for ( int i =0 ; i < m ; i++) {
+				for ( int j = 0 ; j < n ; j++)
+					System.out.print(b[i][j] + "  " ) ;
+				System.out.println() ;}
+			System.out.println("The Sum of Matrix:") ;
+			for ( int i =0 ; i < m ; i++) {
+				for ( int j = 0 ; j < n ; j++)
+					System.out.print(a[i][j]+b[i][j] + "    " ) ;
+				System.out.println() ;}
+			}
+		else{
+			System.out.println(" These Matrices cannot be added....");}		
+			
+		}
+}
