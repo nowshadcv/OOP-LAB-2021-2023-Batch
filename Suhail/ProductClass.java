840c1e177f63f4bde39d9b42f5c13df4a199bc13
suhailk750 <100553721+suhailk750@users.noreply.github.com>
2022-04-07 11:58:29 +0530

    Add files via upload

---
+++ Suhail/ProductClass.java
@@ -0,0 +1,57 @@
+package product;
+
+public class Product {
+	String pname,pcode ;
+	int price ;
+	public Product() {}
+	public Product(String pname, String pcode, int price) {
+			this.pname = pname;
+			this.pcode = pcode;
+			this.price = price; }
+	public void setPname(String pname) {
+			this.pname = pname; }
+	public String getPcode() {
+		return pcode;}
+	public String getPname() {
+		return pname ;}
+	public void setPcode ( String pcode ) {
+		this.pcode = pcode ;}
+	public int getPrice(){
+		return price ;}
+	public void setPrice(int price) {
+		this.price = price ;}
+		public void display( ) {
+			System.out.println("pcode :" + this.pcode);
+			System.out.println("pname :" + this.pname);
+			System.out.println("price :" + this.price + "\n") ;
+			}
+		}
+
+
+package product;
+
+public class Cars {
+	
+	int temp,largest;
+
+	public static void main(String[] args) {
+		Product p1 = new Product();
+		p1.pcode ="cars";
+		p1.pname = "jaguar";
+		p1.price =10000;
+		System.out.println("the p1:");
+		p1.display();
+		
+		Product p2 = new Product("BMW" , "i8", 30000);
+		System.out.println("the string p2:");
+		p2.display();
+		
+		Product p3 = new Product("suzuki","swift",100000);
+		System.out.println("the p3:");
+		p3.display();
+		
+		Product p = p3.getPrice()<(p1.price<p2.price?p1.price:p2.price)?p3: (p1.price<p2.price?p1:p2);
+		System.out.println("\nDisplaying product with lowest price:" );
+		p.display();
+	}
+}
