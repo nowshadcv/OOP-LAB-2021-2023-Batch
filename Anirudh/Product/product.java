57f33fb0c2f2201d61eb3c0d8a8eacbd40db33d8
Anirudh ap <100722197+anirudhpaveri@users.noreply.github.com>
2022-04-07 12:06:43 +0530

    Add files via upload

---
+++ Anirudh/Product/product.java
@@ -0,0 +1,35 @@
+package product;
+
+public class Product {
+	String pname,pcode;
+	int price;
+	public Product(){}
+	public Product(String pname,String pcode,int price){
+		this.pname=pname;
+		this.pcode=pcode;
+		this.price=price;
+	}	
+	public void setPname(String pname){
+		this.pname=pname;
+	}
+	public void setPcode(String pcode){
+		this.pcode=pcode;
+	}
+	public void setPrice(int price){
+		this.price=price;
+	}
+	public String getPname(String pname){
+		return pname;
+	}
+	public String getPcode(String pcode){
+		return pcode;
+	}
+	public int getPrice(int price){
+		return price;
+	}
+	public void display(){
+		System.out.println("pcode :" + this.pcode);
+		System.out.println("pname :" + this.pname);
+		System.out.println("price :" + this.price + "/n");
+	}
+}	
