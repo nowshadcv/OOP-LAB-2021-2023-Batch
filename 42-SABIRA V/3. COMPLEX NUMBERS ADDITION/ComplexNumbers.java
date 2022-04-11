32c2c3217c45b34e1a0dc0c747388f7308865c95
SABIRASEHEER <100561938+SABIRASEHEER@users.noreply.github.com>
2022-04-11 14:54:50 +0530

    Add files via upload

---
+++ 42-SABIRA V/3. COMPLEX NUMBERS ADDITION/ComplexNumbers.java
@@ -0,0 +1,28 @@
+package complex_number;
+
+
+
+public class ComplexNumber {
+ 
+	double real,image;
+	ComplexNumber (double r,double i){
+		real=r;
+		image=i;
+	}
+	public static ComplexNumber sum(ComplexNumber c1,ComplexNumber c2){
+		ComplexNumber temp=new ComplexNumber(0,0);
+		temp.real=c1.real+c2.real;
+		temp.image=c1.image+c2.image;
+		return temp;
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		ComplexNumber c1=new ComplexNumber(5.5,4);
+		ComplexNumber c2=new ComplexNumber(1.2,3.5);
+		ComplexNumber temp=sum(c1,c2);
+		System.out.printf("Sum is: " + temp.real + "+" + temp.image + "i");
+	}
+
+}
