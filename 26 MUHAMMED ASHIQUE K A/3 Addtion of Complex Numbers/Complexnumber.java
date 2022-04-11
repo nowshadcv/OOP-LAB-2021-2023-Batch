12a9420a8ea6c91593dcb1c07f51445756727b3b
ashique1213 <100710114+ashique1213@users.noreply.github.com>
2022-04-11 15:29:04 +0530

    DONE

---
+++ 26 MUHAMMED ASHIQUE K A/3 Addtion of Complex Numbers/Complexnumber.java
@@ -0,0 +1,22 @@
+public class Complexnumber {
+	
+	double real , img;
+	
+	Complexnumber(double r,double i){
+		real = r ; img = i ;
+	}
+public static Complexnumber sum(Complexnumber c1, Complexnumber c2){
+	Complexnumber temp = new Complexnumber(0, 0);
+	temp.real = c1.real + c2.real;
+	temp.img = c1.img +c2.img;
+	return temp;
+}
+public static void main(String[] args){
+	Complexnumber c1 =new Complexnumber(5.5,4);
+	Complexnumber c2 =new Complexnumber(1.2,3.5);
+	Complexnumber temp =sum ( c1 , c2);
+	System.out.print("Sum is :"+temp.real +"+"+ temp.img + "i");
+	
+}
+}
+
