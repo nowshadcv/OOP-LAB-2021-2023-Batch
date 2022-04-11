ea58f92637d970103538f34943f1079285958158
shirin-hyder <100613585+shirin-hyder@users.noreply.github.com>
2022-04-11 15:35:20 +0530

    Add files via upload

---
+++ 39-P.SHIRIN/SYMMETRIC ADDITION/MATRIX SYMMETRIC.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class Symmetricmatrix {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter the number of rows: ");
+		int rows = sc.nextInt();
+		System.out.print("Enter the number of columns: ");
+		int cols = sc.nextInt();
+		int matrix[][] = new int[rows][cols];
+		System.out.println("Enter the elements of matrix : ");
+		for (int i=0;i<rows; i++)
+			for (int j=0; j<cols; j++)
+				matrix[i][j]=sc.nextInt();
+		sc.close();
+		System.out.println("The given matrix is :");
+		for (int i=0; i<rows; i++){
+			for(int j=0; j<cols; j++)
+				System.out.print(matrix[i][j]+ "\t");
+			System.out.println();} 
+	
+	if (rows!=cols)
+		System.out.println("The given matrix is not sqaure matrix");
+	else {
+	boolean symmetric = true;
+	for (int i=0; i<rows; i++)
+		for(int j=0; j<cols; j++)
+	if(matrix[i][j] != matrix[j][i])
+	{
+	symmetric= false; 
+	break;
+	}
+	if(symmetric)
+		System.out.println("The given matrix is symmetric");
+	else
+		System.out.println("The given matrix is not symmetric.");
+
+	}
+}
+}
