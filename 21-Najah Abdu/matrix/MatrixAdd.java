b39c55665cb2a526e807a5ea9662e2be22e0855f
najahabdu1 <100780390+najahabdu1@users.noreply.github.com>
2022-04-11 15:19:29 +0530

    first

---
+++ 21-Najah Abdu/matrix/MatrixAdd.java
@@ -0,0 +1,60 @@
+package matrix;
+import java.util.Scanner;
+public class MatrixAdd {
+
+	public static void main(String[] args) {
+		int p,q,m,n;
+		Scanner s = new Scanner (System.in);
+		System.out.print ("Enter number of rows in matrix A:");
+		p = s.nextInt();
+		System.out.print("Enter number of columns in matrix A");
+		q = s.nextInt();
+		System.out.print("Enter the number of rows matrix B");
+		m = s.nextInt();
+		System.out.print("Enter number of columns in Matrix B ");
+		n = s.nextInt();
+		
+		if (p==m && q==n)
+		{
+			int a[][] = new int[p][q];
+			int b[][] = new int[m][n];
+			
+			
+			System.out.println("Enter the elements of Matrix A:");
+			for(int i=0;i<p;i++)
+				for (int j=0;j<q;j++)
+					a[i][j] = s.nextInt();
+			System.out.println("Enter the elements of Matrix B:") ;
+			for (int i=0;i<m;i++)
+				for(int j = 0 ; j < n ; j++)
+					b[i][j] = s.nextInt();
+					System.out.println("Matrix A");
+			
+			for(int i=0;i<m;i++){
+				for(int j=0;j<q;j++)
+					System.out.print((a[i][j])+" ");
+				System.out.println();}
+			System.out.println("Matrix B");
+			for(int i=0;i<m;i++){
+				for (int j=0;j<n;j++)
+					System.out.print(b[i][j] + "  ");
+				System.out.println();}
+			System.out.println("The Sum Matrix:");
+			for(int i=0;i<m;i++){
+				for (int j=0; j<n;j++)
+					System.out.print(a[i][j]+b[i][j]+ "   ");
+				System.out.println();}
+				
+			}
+			
+			
+		
+		else
+		{
+			System.out.println("These matrices cannot be added");}
+		
+				
+				
+				
+				}
+}
