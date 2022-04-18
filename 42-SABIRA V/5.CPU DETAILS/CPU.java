ef289c99b4f4653597bf4d4f425779c9bcd585a7
SABIRASEHEER <100561938+SABIRASEHEER@users.noreply.github.com>
2022-04-18 14:47:01 +0530

    Add files via upload

---
+++ 42-SABIRA V/5.CPU DETAILS/CPU.java
@@ -0,0 +1,21 @@
+package cpu;
+
+public class CPU {
+	double price;
+	class Processor{
+		double cores;
+		String manufacturer;
+		double getCache (){
+			return 4.3;
+			
+		}
+	}
+	static class RAM{
+		double memory;
+		String manufacturer;
+		double getClockSpeed(){
+			return 5.5;
+		}
+	}
+
+}
---
+++ 42-SABIRA V/5.CPU DETAILS/cpudetails.java
@@ -0,0 +1,15 @@
+package cpu;
+
+public class CPUDetails {
+
+	public static void main(String[] args) {
+		CPU cpu = new CPU();
+		CPU.Processor processor=cpu.new Processor();
+		CPU.RAM ram=new CPU.RAM();
+		System.out.println("processor cache="+ processor.getCache());
+		System.out.println("ram clock sped="+ram.getClockSpeed());
+		
+
+	}
+
+}
