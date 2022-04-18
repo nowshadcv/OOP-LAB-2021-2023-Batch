34e637d92c208ec9eb1614e403135f2db7a3e674
safnashan <103172193+safnashan@users.noreply.github.com>
2022-04-18 15:23:34 +0530

    Add files via upload

---
+++ 43 SAFNA.PP/CPU DETAILS/CPUDetails.java
@@ -0,0 +1,13 @@
+package cpu;
+
+public class CPUDetails {
+
+	public static void main(String[] args) {
+		CPU cpu = new CPU();
+		CPU.Processor processor = cpu.new Processor();
+		CPU.RAM ram = new CPU.RAM() ;
+		System.out.println("processor cache = " + processor.getCache());
+		System.out.println("RAM Clock speed = " + ram.getClockSpeed());
+	}
+
+}
