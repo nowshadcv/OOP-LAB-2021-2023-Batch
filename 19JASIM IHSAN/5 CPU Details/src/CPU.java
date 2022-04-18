13b202f349581c77a19a5b0edbf649ed5cd03a1d
Jasim Ihsan <68841778+lucidethinker@users.noreply.github.com>
2022-04-18 15:16:13 +0530

    inline inputting is done ..

---
+++ 19JASIM IHSAN/5 CPU Details/src/CPU.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner; 
+
+public class CPU {
+	double price;
+	class Processor{
+		double cores;
+		String manufacturer;
+		double getCache(){
+		     System.out.print("Enter the Desired Value : ");  
+		        Scanner sc = new Scanner(System.in);  
+		        double i = sc.nextDouble(); 
+			return i ;
+		}
+	}
+		static class RAM{
+			double memory;
+			String manufacturer;
+			double getClockSpeed()
+			{
+				 System.out.print("Enter the Second Desired Value : ");  
+			        Scanner sc = new Scanner(System.in);  
+			        double b = sc.nextDouble(); 
+				
+				return b ;}}}
+			
+		
+
