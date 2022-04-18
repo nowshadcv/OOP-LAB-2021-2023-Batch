3fc01c75095df1a25f33c6df1e89713fb1114946
jalmk001 <100552237+jalmk001@users.noreply.github.com>
2022-04-18 15:21:29 +0530

    Add files via upload

---
+++ 18JALVA/CPU/CPU.java
@@ -0,0 +1,20 @@
+public class CPU {
+	double price;
+	class Processor{
+		double cores;
+		String manufacturer;
+		double getCache (){
+			return 9.2;
+
+		}
+	}
+	static class RAM{
+		double memory;
+		String manufacturer;
+		double getClockSpeed(){
+			return 5.9;
+		}
+	}
+
+}
+
