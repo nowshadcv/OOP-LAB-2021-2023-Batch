bb941d81a201e1b9dfbd90b461183f8616efd2dc
haafil <100757180+haafil@users.noreply.github.com>
2022-04-18 15:21:01 +0530

    Add files via upload

---
+++ 25 MOHAMMED HAFIL K P/05. CPU/CPUDetails.java
@@ -0,0 +1,13 @@
+
+public class CPUDetails {
+
+	public static void main(String[] args) {
+		CPU cpu = new CPU();
+		CPU.Processor processor = cpu.new Processor();
+		CPU.RAM ram = new CPU.RAM();
+		
+		System.out.println("Processor Cache = " + processor.getCache());
+		System.out.println("Processor Clock speed = " + ram.getClockSpeed());
+	}
+
+}
