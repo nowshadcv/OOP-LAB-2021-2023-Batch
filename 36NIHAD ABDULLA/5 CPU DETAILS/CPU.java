42195b73d8269f4f3b1d780084779b181c6ed273
Nihad-Abdulla <100753393+Nihad-Abdulla@users.noreply.github.com>
2022-04-18 15:33:38 +0530

    initial commit

---
+++ 36NIHAD ABDULLA/5 CPU DETAILS/CPU.java
@@ -0,0 +1,15 @@
+public class CPU {
+	double price;
+	class Processor{
+		double cores;
+		String manufacturer;
+		double getCache( ) {
+			return 4.3 ;} }
+	static class RAM{
+		double memory;
+		String manufacture;
+		double getClockSpeed() {
+			return 5.5 ;}
+		}
+	}
+
