5b1fcd05879845ee9f4c86d7c97a380be61de559
Athira-nair-padikkal <100551914+Athira-nair-padikkal@users.noreply.github.com>
2022-04-18 14:58:08 +0530

    ALL DONE

---
+++ 06ATHIRA/5.CPU/src/cpu/CPU.java
@@ -0,0 +1,16 @@
+package cpu;
+
+public class CPU {
+	double price ;
+	class processor {
+		double cores ;
+		String manufacturer;
+		double getCache () {
+			return 4.3 ;}}
+	static class RAM{
+		double memory ;
+		String manufacturer ;
+		double getClockSpeed() {
+			return 5.5 ;}}}
+
+
