5ae107bb03db43702c4f89c1eda62c334d710999
PARVATHIPNAIR <100523745+PARVATHIPNAIR@users.noreply.github.com>
2022-04-21 11:58:39 +0530

    Add files via upload

---
+++ 38PARVATHI/5CPU/CPU.java
@@ -0,0 +1,15 @@
+package cpu;
+
+public class CPU {
+	double price;
+	class Processor {
+		double cores;
+		String manufactures;
+		double getCache ( ){
+			return 4.3;}}
+	static class RAM{
+		double memory ;
+		String manufacturer;
+		double getClockSpeed() {
+			return 5.5;}}}
+
