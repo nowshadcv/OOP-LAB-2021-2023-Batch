eaa8a02492d2d1dd98cfac217c74d6b4ec9cc86e
kt-riya <100552231+kt-riya@users.noreply.github.com>
2022-04-21 11:03:02 +0530

    Add files via upload

---
+++ 41 RIYA/05-CPU DETAILS/CPU.java
@@ -0,0 +1,20 @@
+
+public class CPU {
+	double price;
+	class Processor{
+		double cores;
+		String manufacturer;
+		double getCache (){
+			return 4.3;
+
+		}
+	}
+	static class RAM{
+		double memory;
+		String manufacturer;
+		double getClockSpeed(){
+			return 5.5;
+		}
+	}
+
+}
