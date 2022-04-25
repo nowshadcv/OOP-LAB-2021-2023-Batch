aa2aa3608af8e0120cf193eb5af4aada0e6cf9cb
Nihad-Abdulla <100753393+Nihad-Abdulla@users.noreply.github.com>
2022-04-25 15:38:15 +0530

    Add files via upload

---
+++ 36NIHAD ABDULLA/8 METHOD OVERLOADING/OverLoadDemo.java
@@ -0,0 +1,18 @@
+package methodoverloading;
+
+public class OverLoadDemo {
+	void area(float x){
+		System.out.println("The area of the square is "+Math.pow(x, 2)+"sq units");}
+		void area(float x,float y){
+			System.out.println("the area of the rectangle is "+x*y+"sq units");}
+		void area(double x){
+			double z =3.14*x*x;
+			System.out.println("The area the circle is "+z+"sq units");
+			
+		}
+	
+
+	
+	
+
+	}
