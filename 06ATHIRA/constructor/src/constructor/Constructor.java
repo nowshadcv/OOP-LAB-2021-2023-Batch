2783896382227d5db92627633c7ce75e9cfb5e7d
Athira-nair-padikkal <100551914+Athira-nair-padikkal@users.noreply.github.com>
2022-05-05 11:46:34 +0530

    ALL DONE

---
+++ 06ATHIRA/constructor/src/constructor/Constructor.java
@@ -0,0 +1,18 @@
+package constructor;
+
+public class Constructor {
+	int x;
+ 
+public Constructor(){
+	x=5;
+}
+
+	public static void main(String[] args) {
+		Constructor myObj = new Constructor();
+		System.out.println(myObj.x);
+		
+		
+
+	}
+
+}
