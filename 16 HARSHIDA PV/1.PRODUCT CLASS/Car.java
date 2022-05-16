1b54bcab3e92c55f2ba2994260121c9420ca7eb9
Harshidapv <100551644+Harshidapv@users.noreply.github.com>
2022-05-16 16:32:02 +0530

    Add files via upload

---
+++ 16 HARSHIDA PV/1.PRODUCT CLASS/Car.java
@@ -0,0 +1,29 @@
+package product;
+
+public class Car {
+
+	public static void main(String[] args) {
+		Product p1 = new Product();
+		p1.pcode = "Car123";
+		p1.pname = "Benz";
+		p1.price = 10000;
+		System.out.println("Displaying p1:");
+		p1.display();
+		
+		
+		Product p2 = new Product("Jaguar" , "Car426" , 25000 );
+		System.out.println("Displaying p2: " );
+		p2.display() ;
+		
+		
+		Product p3 = new Product ("Maruthi", "Car800", 50000 );
+		System.out.println( "Displaying p3: ");
+		p3.display( ) ;
+		Product p = p3.getPrice(0)<(p1.price<p2.price?p1.price:p2.price)?p3: (p1.price<p2.price?p1:p2);
+
+		System.out.println("\nDisplaying product with lowest price:" );
+		p.display();
+
+	}
+
+}
