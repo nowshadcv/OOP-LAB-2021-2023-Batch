0ac31ab9a6411e6d9567b073e4c32f057109f3a6
NIDHAP <100557602+NIDHAP@users.noreply.github.com>
2022-05-16 15:36:59 +0530

    Add files via upload

---
+++ 35 NIDHA POOZHIKAL/5 CPU DETAILS/CPUDetails.java
@@ -0,0 +1,24 @@
+public class CPUDetails {
+
+	public static void main(String[] args) {
+				CPU cpu=new CPU();
+				CPU.Processor processor =cpu.new Processor() ;
+				CPU.RAM ram = new CPU.RAM();
+				System.out.println("Processor Cach = " + processor.getCache());
+				System.out.println("RAM Clock speed = " + ram.getClockSpeed());
+
+			}
+
+		}
+
+    © 2022 GitHub, Inc.
+
+    Terms
+    Privacy
+    Security
+    Status
+    Docs
+    Contact GitHub
+    Pricing
+    API
+    Traini
