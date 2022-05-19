ae7f528abc09eef05e630575b6619507473e838d
Gopikagireesh01 <100705078+Gopikagireesh01@users.noreply.github.com>
2022-05-19 17:04:09 +0530

    Add files via upload

---
+++ 15GOPIKAGIREESH/1-PRODUCT/Productc.java
@@ -0,0 +1,17 @@
+
+public class Productc {
+	String pname;
+	public Productc() {
+		pname="laptop";
+	
+	}
+
+	public static void main(String[] args) {
+		Productc s= new Productc();
+		System.out.println("The product name is : "+s.pname);
+		
+		
+
+	}
+
+}
