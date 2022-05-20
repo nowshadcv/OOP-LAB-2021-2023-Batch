31c61a8a975b6c6940688b013c40a9096bf4c93d
sourav satheesh <100605271+sourav-v@users.noreply.github.com>
2022-05-20 14:23:51 +0530

    Add files via upload

---
+++ 53 SOURAV.V/9 CONCEPT OF ARRAY OBJECT/EMPLOYEE.JAVA
@@ -0,0 +1,18 @@
+package arrayobjects;
+
+public class Employee {
+	int eNo,eSalary;
+	String eName;
+	public Employee(){}
+	public Employee(int no, int sal, String name){
+		eNo =no;
+		eSalary=sal;
+		eName=name;
+		
+	}
+	public void showData(){
+		System.out.print("EmpId = "+eNo+" "+"Name = "+eName+" "+"salary = "+ eSalary);
+		System.out.println();
+	}
+
+}
