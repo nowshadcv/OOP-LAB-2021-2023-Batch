c9f94ba066102dad767b4359287e3a6102622f69
asnanasrin <100749222+asnanasrin@users.noreply.github.com>
2022-05-24 15:58:46 +0530

    commit

---
+++ 05-ASNA NASRIN/11.INHERITANCE/TeacherArrObjects.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class TeacherArrObjects{
+	public static void main(String[] args) {
+		System.out.println("Enter number of Teachers:");
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		System.out.println("Enter Teacher details one by one...");
+		Teacher teacher[]=new Teacher[n];
+		Scanner scT=new Scanner(System.in);
+		int tid;String name;float salary;String address;
+		String department;String subject;
+		for(int i=0;i<n;i++){
+			System.out.println("Enter "+i+"teacher details...");
+			System.out.println("Enter teacher id(integer):");
+			tid=scT.nextInt();
+			System.out.println("Enter teacher name(String):");
+			name=scT.next();
+			System.out.println("Enter teacher salary(float):");
+			salary=scT.nextFloat();
+			System.out.println("Enter teacher address(String):");
+			address=scT.next();
+			System.out.println("Enter teacher department(String):");
+			department=scT.next();
+			System.out.println("Enter teacher subject(String):");
+			subject=scT.next();
+			Teacher t=new Teacher(tid,name,salary,address,department,subject);
+			teacher[i]=t;}
+		System.out.println("Teachers are:\n");
+		for(Teacher x:teacher) {
+			x.display();
+			System.out.println("\n");
+		}
+	}
+}
