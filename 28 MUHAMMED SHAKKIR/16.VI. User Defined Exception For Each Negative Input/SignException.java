44cf0bf5f7b60e949ab97869e3217676a8fa5b30
mvmshakkir <100688841+mvmshakkir@users.noreply.github.com>
2022-05-30 15:18:33 +0530

    committ

---
+++ 28 MUHAMMED SHAKKIR/16.VI. User Defined Exception For Each Negative Input/SignException.java
@@ -0,0 +1,27 @@
+package avg;
+
+
+
+import java.util.Scanner;
+class MyException extends Exception{
+
+	public MyException(String str){System.out.println(str);}
+}
+public class SignException {
+	public static void main(String[] args){
+		System.out.println("inputn :");
+		Scanner s=new Scanner(System.in);
+		int N=s.nextInt();
+		int sum=0;
+		for(int i=0;i<N;i++){
+			System.out.println("input number"+(i+1)+":");
+			int num=s.nextInt();
+			try{
+				if(num<0) throw new MyException("number is negative ...!");
+			}catch(MyException m){System.out.println(m);i--;continue;}
+			sum=sum+num;}
+		System.out.println("Average of entered numbers :"+(double)sum/N);
+			}
+	
+
+}
