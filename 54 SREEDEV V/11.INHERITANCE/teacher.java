ea2d1063da8a2e7150b2eca17d6a89ae4b927923
Devn <100791152+Sreedev-v@users.noreply.github.com>
2022-05-30 15:51:21 +0530

    Add files via upload

---
+++ 54 SREEDEV V/11.INHERITANCE/teacher.java
@@ -0,0 +1,19 @@
+package empteach;
+
+public class Teacher extends EmployeeT {
+	String department,subject;
+	Teacher(int empid,String name,float salary,String address,String department,String subject){
+		super(empid,name,salary,address);
+		this.department=department;
+		this.subject=subject;
+	}
+	public void display() {
+		System.out.println(" Teacher id = "+empid);
+		System.out.println(" Teacher name = "+name);
+		System.out.println(" Teacher salary = "+salary);
+		System.out.println(" Teacher address = "+address);
+		System.out.println(" Teacher department = "+department);
+		System.out.println(" Teacher subject = "+subject);
+	}
+
+}
