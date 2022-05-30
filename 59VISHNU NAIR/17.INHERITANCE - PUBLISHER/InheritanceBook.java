a3c52799898ff1733f428b413e869345545a3c3f
vis777 <82163417+vis777@users.noreply.github.com>
2022-05-30 16:04:58 +0530

    Add files via upload

---
+++ 59VISHNU NAIR/17.INHERITANCE - PUBLISHER/InheritanceBook.java
@@ -0,0 +1,39 @@
+class Publisher {
+	String publisher ;
+	Publisher ( String publi ){
+	this .publisher = publi ;}}
+
+class Book {
+	String name ;
+	Publisher publisher ;
+	Book ( ) { }
+	public Book ( String name , Publisher publisher ) {
+		this.name = name;
+		this.publisher = publisher ;}}
+
+class Literature extends Book {
+	String Lit_type = " Literature ";
+	Literature ( String name , Publisher publisher ) {
+		super ( name , publisher ) ;}
+	void display( ) {
+		System.out.println ("Name: " +super .name ) ;
+		System.out.println ("Type: " +this .Lit_type ) ;
+		System.out.println ("Publisher: " +super .publisher . publisher ) ;}}
+
+class Fiction extends Book {
+	String Lit_type = " Fiction" ;
+	Fiction  ( String name ,Publisher publisher) {
+		super ( name , publisher) ;}
+	void display( ) {
+		System.out.println ("Name: " +super .name ) ;
+		System.out.println ("Type: " +this .Lit_type ) ;
+		System.out.println ("Publisher: " +super .publisher . publisher ) ;}}
+
+public class InheritanceBook {
+	public static void main(String[] args) {
+		Publisher lp = new Publisher ( "S . Chand" ) ;
+		Literature l = new Literature ( "As you like it" ,lp);
+		l .display( ) ;
+		Publisher fp = new Publisher ( "Tata McGraw Hill ") ;
+		Fiction f = new Fiction ( "Tempest " ,fp) ;
+		f . display ( ) ;}}
