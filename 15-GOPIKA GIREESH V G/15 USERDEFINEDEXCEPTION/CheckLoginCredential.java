47b2da8084cc3bb3c12377c2be118761a37cce79
Gopikagireesh01 <100705078+Gopikagireesh01@users.noreply.github.com>
2022-05-30 15:25:35 +0530

    Add files via upload

---
+++ 15-GOPIKA GIREESH V G/15 USERDEFINEDEXCEPTION/CheckLoginCredential.java
@@ -0,0 +1,36 @@
+package userdefinedexception;
+import java.util.Scanner;
+
+class UsernameException extends Exception {
+	public UsernameException(String msg) { super(msg); }}
+
+class PasswordException extends Exception {
+	public PasswordException(String msg) { super(msg); }}
+
+public class CheckLoginCredential {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String username,password;
+		System.out.print("Enter username : ");
+		username = s.nextLine();
+		System.out.println("Enter password : ");
+		password = s.nextLine();
+		int length = username.length();
+		try {
+			if(length < 6)
+				throw new UsernameException("Username must be greater than 6 characters");
+			else if (!username.equals("application"))
+				throw new UsernameException("Username incorrect....!");
+			else if(!password.equals("mca"))
+				throw new PasswordException("Incorrect password..! Type correct password");
+			else
+				System.out.println("Login successful !!!");
+		} catch (UsernameException u) { u.printStackTrace();}
+			catch (PasswordException p) { p.printStackTrace();}
+				finally { System.out.println("The finally is always excecuted... ");}}
+		}
+
+	
+
+
