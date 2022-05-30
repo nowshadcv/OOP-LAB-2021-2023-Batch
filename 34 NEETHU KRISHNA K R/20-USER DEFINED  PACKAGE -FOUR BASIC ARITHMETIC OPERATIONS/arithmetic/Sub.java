6153efde34902db1634a139a7918e4fc406084de
Neethukrishna-KR <100551835+Neethukrishna-KR@users.noreply.github.com>
2022-05-30 08:38:05 -0700

    Add files via upload

---
+++ 34 NEETHU KRISHNA K R/20-USER DEFINED  PACKAGE -FOUR BASIC ARITHMETIC OPERATIONS/arithmetic/Sub.java
@@ -0,0 +1,9 @@
+package operations;
+
+public class Sub implements Subtraction {
+	private int num1,num2;
+	public Sub(int n1,int n2){num1=n1; num2=n2;}
+	public double sub() {return(num1-num2);}
+
+
+}
