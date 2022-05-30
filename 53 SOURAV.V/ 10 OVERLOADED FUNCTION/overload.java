519b06923adde71d6a9d15acd7d339df8f66ea90
sourav satheesh <100605271+sourav-v@users.noreply.github.com>
2022-05-30 15:16:12 +0530

    Add files via upload

---
+++ 53 SOURAV.V/ 10 OVERLOADED FUNCTION/overload.java
@@ -0,0 +1,13 @@
+package methodoverload;
+
+public class Overload {
+
+	public static void main(String[] args) {
+		OverloadDemo ob = new OverloadDemo();
+		ob.area(5);
+		ob.area(11, 12);
+		ob.area(2.5);
+
+	}
+
+}
