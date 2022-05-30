7d1c837efe2038693beb2787040823cf170382c7
SEENA-DEVASSIA <100551826+SEENA-DEVASSIA@users.noreply.github.com>
2022-05-30 15:23:06 +0530

    Add files via upload

---
+++ 45SEENA/14-USER DEFINED GRAPHIC PACKAGES/Rectangle.java
@@ -0,0 +1,14 @@
+package figures;
+
+public class Rectangle implements FigureRt{
+	private int length,breadth;
+	public Rectangle(int l,int b) {length=l; breadth=b;}
+	public int perimeter() {return 2*(length+breadth);}
+	public int  area() {return(length*breadth);
+		
+	}
+	
+	
+	
+
+}
