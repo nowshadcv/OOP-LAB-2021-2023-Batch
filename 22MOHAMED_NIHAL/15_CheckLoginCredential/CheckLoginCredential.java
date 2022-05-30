1d1fff85e8f308fffaf77b13831234ac67e8617b
Mohamed Nihal <47349903+nihalnazim@users.noreply.github.com>
2022-05-30 15:02:23 +0530

    Add files via upload

---
+++ 22MOHAMED_NIHAL/15_CheckLoginCredential/CheckLoginCredential.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+class UsernameException extends Exception{
+	public UsernameException(String msg){ 
+		super(msg);	
+	}
+}
+
+class PasswordException extends Exception{
+	public PasswordException(String msg){ 
+		super(msg);	
+		}
+}
+
+public class CheckLoginCredential {
+	public static void main(String[] args) {
+		Scanner s = new Scanner (System.in);
+		String username , password;
+		System.out.print("Enter username");
+		username=s.nextLine();
+		System.out.print("Enter the password");
+		password=s.nextLine();
+		int length = username.length();
+		try {
+			if (length < 6)
+				throw new UsernameException("Username must be greater than 6 charater..!");
+			else if (!username.equals("application" ))
+				throw new UsernameException("Username incorrect...!");
+			else if (!password.equals("mca"))
+				throw new PasswordException("Incorrect Password..! Type correct password.");
+			else
+				System.out.println("Login Successfull !!!");
+		}
+		catch(UsernameException u)
+		{
+			u.printStackTrace();
+			}
+		
+		catch(PasswordException p)
+		{
+			p.printStackTrace();
+			}
+		finally {
+			System.out.println("The finally is always executed..");
+		}
+	}
+}
