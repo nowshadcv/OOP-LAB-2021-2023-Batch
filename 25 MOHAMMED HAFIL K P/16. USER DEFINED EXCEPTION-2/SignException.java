173cbb1b8d350f0aa7c3589652dc984dd55ca5b2
haafil <100757180+haafil@users.noreply.github.com>
2022-05-30 15:45:12 +0530

    Add files via upload

---
+++ 25 MOHAMMED HAFIL K P/16. USER DEFINED EXCEPTION-2/SignException.java
@@ -0,0 +1,34 @@
+package exception;
+import java.util.Scanner;
+
+class MyException extends Exception {
+	public MyException(String str) {
+		System.out.println(str);
+	}
+}
+
+public class SignException {
+
+	public static void main(String[] args) {
+		System.out.println("Input N : ");
+		Scanner s = new Scanner(System.in);
+		int N = s.nextInt();
+		int sum = 0;
+
+		for(int i = 0; i < N; i++) {
+			System.out.println("Input number" + (i + 1)+ ": ");
+			int num = s.nextInt();
+			try {
+				if(num < 0)
+					throw new MyException("Number is Negative..!");
+			} catch(MyException m) {
+				System.out.println(m);
+				i--;
+				continue;
+			}
+			sum = sum + num;
+		}
+		System.out.println("Avarage of entered numbers: " + (double)sum / N);
+	}
+
+}
