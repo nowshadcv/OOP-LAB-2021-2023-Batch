63492042aae6c9a995aba660cf7719aba888b32b
vis777 <82163417+vis777@users.noreply.github.com>
2022-05-30 19:04:21 +0530

    Add files via upload

---
+++ 59VISHNU NAIR/20.USERDEFINED PACKAGES ARITHMETIC OPERATIONS/operations/Mul.java
@@ -0,0 +1,9 @@
+package operations;
+
+public class Mul implements Multiplication {
+	private int num1,num2;
+	public Mul(int n1,int n2){num1=n1; num2=n2;}
+	public int mul() {return(num1*num2);}
+
+
+}
