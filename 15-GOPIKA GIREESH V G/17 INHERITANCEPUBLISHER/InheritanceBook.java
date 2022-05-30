eb0f858cd522ddfe2169ec7411dc7197c23c0477
Gopikagireesh01 <100705078+Gopikagireesh01@users.noreply.github.com>
2022-05-30 15:57:22 +0530

    Add files via upload

---
+++ 15-GOPIKA GIREESH V G/17 INHERITANCEPUBLISHER/InheritanceBook.java
@@ -0,0 +1,43 @@
+package publisher;
+class Publisher{
+	String publisher;
+	Publisher(String publi){
+		this.publisher=publi;}}
+class Book{
+	String name;
+	Publisher publisher;
+	Book( ) { }
+	public Book(String name,Publisher publisher) {
+		this.name=name;
+		this.publisher=publisher;}}
+class Literature extends Book{
+	String Lit_type="Literature";
+	Literature(String name,Publisher publisher){
+		super(name,publisher);}
+	void display() {
+		System.out.println("name:"+super.name);
+		System.out.println("type:"+this.Lit_type);
+		System.out.println("publisher:"+this.publisher.publisher);}}
+
+class Fiction extends Book{
+	String Lit_type="Fiction";
+	Fiction(String name,Publisher publisher){
+		super(name,publisher);}
+	void display() {
+		System.out.println("name:"+super.name);
+		System.out.println("type:"+this.Lit_type);
+		System.out.println("publisher:"+this.publisher.publisher);}}
+
+public class InheritanceBook {
+
+	public static void main(String[] args) {
+		Publisher lp=new Publisher("s.chand");
+		Literature l=new Literature("As you like it",lp);
+		l.display();
+		Publisher fp=new Publisher("tata mcgraw hill");
+		Fiction f=new Fiction("Tempest",fp);
+		f.display();}}
+
+
+
+
