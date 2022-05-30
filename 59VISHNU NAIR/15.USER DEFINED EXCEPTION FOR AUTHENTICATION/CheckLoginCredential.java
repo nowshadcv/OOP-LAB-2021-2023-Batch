5387917712db403e758cf9c77d690fdf39abdf19
vis777 <82163417+vis777@users.noreply.github.com>
2022-05-30 15:52:45 +0530

    Add files via upload

---
+++ 59VISHNU NAIR/15.USER DEFINED EXCEPTION FOR AUTHENTICATION/CheckLoginCredential.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+class UsernameException extends Exception {
+	public UsernameException(String msg)
+	{
+		super(msg);
+	}
+}
+class PasswordException extends Exception {
+	public PasswordException(String msg)
+	{
+		super(msg);
+	}
+}
+public class CheckLoginCredential {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String username, password;
+		System.out.println("Enter username :");
+		username = s.nextLine();
+		System.out.println("Enter password :");
+		password = s.nextLine();
+		int length = username.length();
+		try
+		{
+			if(length < 6)
+				throw new UsernameException("Username must be greater than 6 characters..|");
+			else if(!username.equals("application"))
+				throw new UsernameException("Username Incorrect..|");
+			else if(!password.equals("mca"))
+				throw new PasswordException("Incorrect password..! Type correct password.");
+			else
+				System.out.println("Login Successful !!!");
+		}
+		catch (UsernameException u)
+		{
+			u.printStackTrace();
+		}
+		catch (PasswordException p)
+		{
+			p.printStackTrace();
+		}
+		finally
+		{
+			System.out.println("The finally is always executed..");
+		}
+		
+
+	}
+
+}
---
+++ 59VISHNU NAIR/16.USER DEFINED EXCEPTION FOR EACH NEGATIVE INPUT/SignException.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+class MyException extends Exception{
+	public MyException(String str)
+	{
+		System.out.println(str);
+	}
+}
+public class SignException 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Input N : ");
+		Scanner  s = new Scanner(System.in);
+		int N = s.nextInt();
+		int sum = 0;
+			for(int i = 0;i < N; i++)
+			{
+				System.out.println("Input number " + (i+1) + " : ");
+				int num = s.nextInt();
+				try
+				{
+					if(num < 0)  throw new MyException("Number is negative..|");
+				}
+				catch(MyException m)
+				{
+					System.out.println(m);
+					i--;
+					continue;
+			    }
+				sum = sum + num;
+			}
+System.out.println("Average of Entered Numbers : " + (double)sum/N);
+		}
+	}
