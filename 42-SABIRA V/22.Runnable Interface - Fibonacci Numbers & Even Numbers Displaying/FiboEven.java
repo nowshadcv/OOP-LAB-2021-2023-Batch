4cc106d21d5cca65a78401f2c1abbeb274523c37
SABIRASEHEER <100561938+SABIRASEHEER@users.noreply.github.com>
2022-06-09 12:30:06 +0530

    Add files via upload

---
+++ 42-SABIRA V/22.Runnable Interface - Fibonacci Numbers & Even Numbers Displaying/FiboEven.java
@@ -0,0 +1,57 @@
+package fiboeven;
+
+class Fibonacci implements Runnable{
+	String name;
+	Thread t;
+	Fibonacci (String thread){
+		name=thread;
+		t=new Thread(this,name);
+		System.out.println("Fibonacci: "+ t);
+		t.start();
+		
+	}
+	public void run() {
+		int n1=0,n2=1,n3,i,count=10;
+		System.out.print(n1+" "+n2);
+		for(i=2;i<count;++i) {
+			n3=n1+n2;
+			System.out.print(" "+n3);
+			n1=n2;
+			n2=n3;
+		}
+	
+	}
+}
+class Even implements Runnable{
+	String name;
+	Thread t;
+	Even (String thread){
+		name=thread;
+		t=new Thread(this,name);
+		System.out.println("\nEven: "+t);
+		t.start();
+	}
+	public void run() {
+		int number=30;
+		System.out.println("Even upto 30: ");
+		for(int i=1;i<=number; i++) {
+			if(i%2==0) {
+				System.out.print(i+" ");
+			}
+		}
+	}
+}
+public class FiboEven {
+
+	public static void main(String[] args) {
+		new Fibonacci("Fibonacci");
+		new Even("Even");
+		try {
+			Thread.sleep(10000);
+		}catch(InterruptedException e) {
+			System.out.println("main thread interrupter");
+		}
+		System.out.println("\n Main thread exiting.");
+	}
+
+}
