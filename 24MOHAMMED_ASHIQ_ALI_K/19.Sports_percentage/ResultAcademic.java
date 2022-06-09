df64141a8dd07fed274a38f7bf66beb38a08cc43
MOHAMMED ASHIQ ALI K <97211773+ashiq5566@users.noreply.github.com>
2022-06-08 23:50:55 -0700

    Add files via upload

---
+++ 24MOHAMMED_ASHIQ_ALI_K/19.Sports_percentage/ResultAcademic.java
@@ -0,0 +1,32 @@
+
+	class Result extends Student implements Sports1 {
+	 
+	 float per;
+	 Result(String n,String sp,int m1,int m2) {
+	 
+	  super(n,sp,m1,m2);
+	 }
+	 
+	 public void Percent_cal() {
+	 
+	  int tot = score1 + score2;
+	  per = (float)tot / 2;
+	 }
+	 
+	 void display() {
+	 
+	  show();
+	  System.out.println("Percentage = "+per);
+	 }
+	 
+	}
+
+	public class ResultAcademic {
+	 
+	 public static void main (String[] args) {
+	 
+	  Result r = new Result("Aashish","Cricket",75,95);
+	  r.Percent_cal();
+	  r.display();
+	 }
+	}
