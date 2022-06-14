0340777e31e26cf907b4699af5b933e98f4315a0
Neethukrishna-KR <100551835+Neethukrishna-KR@users.noreply.github.com>
2022-06-14 11:32:41 +0530

    Add files via upload

---
+++ 34 NEETHU KRISHNA K R/14-USER DEFINED GRAPHIC PACKAGES/TestPacakge_figures.java
@@ -0,0 +1,39 @@
+package Test;
+import figures.*;
+import java.util.Scanner;
+
+public class TestPacakge_figures {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the side of square: ");
+		int s=sc.nextInt();
+		Square sq=new Square(s);
+		System.out.println("Perimeter : "+sq.perimeter());
+		System.out.println("Area: "+sq.area());
+		System.out.println("Enter the radius of the circle: ");
+		float r=sc.nextInt();
+		Circle ci=new Circle(r);
+		System.out.println("perimeter : "+ci.perimeter());
+		System.out.println("Area : "+ ci.area());
+		System.out.println("Enter the side1 of Triangle: ");
+		int s1=sc.nextInt();
+		System.out.println("Enter the side2 of Triangle: ");
+		int s2=sc.nextInt();
+		System.out.println("Enter the side3 of Triangle: ");
+		int s3=sc.nextInt();
+		Triangle tr=new Triangle(s1,s2,s3);
+		System.out.println("perimeter : "+tr.perimeter());
+		System.out.println("Area: "+tr.area());
+		System.out.println("Enter the length of Rectangle:");
+		int l = sc.nextInt();
+		System.out.println("Enter the breadth of Rectangle:");
+		int b = sc.nextInt();
+		Rectangle rt = new Rectangle(l,b);
+		System.out.println("Perimeter:" + rt.perimeter());
+		System.out.println("Area:" + rt.area());
+
+
+	}
+
+}
