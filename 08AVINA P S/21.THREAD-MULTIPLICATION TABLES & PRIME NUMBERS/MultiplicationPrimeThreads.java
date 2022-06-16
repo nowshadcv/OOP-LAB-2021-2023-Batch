be4101633fc54542452957d2c39acb7cb85ebe69
avina-pulikkal <100551949+avina-pulikkal@users.noreply.github.com>
2022-06-16 11:54:47 +0530

    Add files via upload

---
+++ 08AVINA P S/21.THREAD-MULTIPLICATION TABLES & PRIME NUMBERS/MultiplicationPrimeThreads.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+class MultiplicationT{
+	Thread t1 = new Thread() {
+		public synchronized void run() {
+			Scanner s = new Scanner(System.in);
+			System.out.println("Multipliction table for:");
+			int N=s.nextInt();
+			System.out.printf("Multipliction table of %d \n",N);
+			for(int i=1;i<=10;++i)
+				System.out.printf("Thread1 : %d * %d = %d \n",N,i,N * i);}};}
+	
+class PrimeN{
+	Thread t2 =new Thread() {
+		public  synchronized void run() {
+			Scanner s = new Scanner(System.in);
+			System.out.println("Prime numbers upto:");
+			int N=s.nextInt();
+			System.out.printf("first %d Prime Numbers: \n",N);
+			int count,n=0,i=1,j;
+			while(n<N) {
+				j=1; count=0;
+				while(j<=i) {
+					if(i%j==0) count++; j++;}
+				if(count==2) {
+					System.out.printf("Thread2: %d \n",i); n++;} i++; }}};}
+
+			
+		
+	
+	
+
+public class MultiplicationPrimeThreads {
+
+	public static void main(String[] args) {
+		MultiplicationT ob1 =new MultiplicationT();
+		PrimeN ob2 =new PrimeN();
+		ob1.t1.start();
+		ob2.t2.start();
+	}
+
+}
+
+
