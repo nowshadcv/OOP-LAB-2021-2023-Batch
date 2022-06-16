fd811c6dbe59447250b397a9b63567a9e1d66cfb
jeniyamajeed <100665175+jeniyamajeed@users.noreply.github.com>
2022-06-16 11:19:01 +0530

    commit

---
+++ 20JENIYA/21.MULTIPLICATION TABLES AND PRIME NUMBERS.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+class MultiplicationT
+{
+	Thread t1=new Thread() {
+		public synchronized void run() {
+			Scanner s=new Scanner(System.in);
+			System.out.println("Multiplication Table for: ");
+			int N=s.nextInt();
+			System.out.printf("Multiplication Table of %d \n",N);
+			for(int i=1;i<=10;++i)
+				System.out.printf("Thread1: %d * %d=%d \n",N,i,N*i);}};}
+            
+		class primeN{
+			Thread t2=new Thread()
+					{
+				public synchronized void run()
+				{
+					Scanner s= new Scanner(System.in);
+					System.out.println("Prime numbers upto: ");
+					int N=s.nextInt();
+					System.out.printf("First %d prime numbers: \n",N);
+					int count,n=0,i=1,j;
+					while(n<N) {
+						j=1; count=0;
+						while(j<=i)
+						{
+							if(i%j==0) count++; j++;}
+							if(count==2) {
+								System.out.printf("Thread2:%d \n",i); n++;} i++;}}};}
+						
+					
+				
+					
+		
+	
+
+public class MultiplicationPrimeThreads {
+
+	public static void main(String[] args) {
+		MultiplicationT ob1=new MultiplicationT();
+		primeN ob2=new primeN();
+		ob1.t1.start();
+		ob2.t2.start();
+		
+
+	}
+
+}
