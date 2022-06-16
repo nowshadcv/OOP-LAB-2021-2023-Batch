58e2d295a4c38c91c0b45ba47e6be3aa18cb788d
najahabdu1 <100780390+najahabdu1@users.noreply.github.com>
2022-06-16 07:50:40 -0700

    19th program pushing

---
+++ 32-Najah Abdu/19 - Students and Sports/Sports.java
@@ -0,0 +1,24 @@
+public interface Sports {
+	 void Percent_cal();
+		}
+
+		class Student {
+		 
+		 String name,sports_name;
+		 int roll_no, score1, score2;
+		 Student(String n, String sp, int s1, int s2) {
+		 
+		  name = n;
+		  sports_name = sp;
+		  score1 = s1;
+		  score2 = s2;
+		 }
+		 
+		 void show() {
+		 
+		  System.out.println("Student Name : "+name);
+		  System.out.println("sports_name:"+sports_name);
+		  System.out.println("score1 : "+score1);
+		  System.out.println("score2 : "+score2);
+		 }
+}
