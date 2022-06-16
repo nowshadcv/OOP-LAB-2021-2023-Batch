02f5575fc929cd09478a2215cebcbcd1570a700b
safnashamli <81615996+safnashamli@users.noreply.github.com>
2022-06-15 23:32:43 -0700

    Add files via upload

---
+++ 44 SAFNA SHAMLI/11.INHERITANCE/EmployeeT.java
@@ -0,0 +1,14 @@
+package program11;
+
+public class EmployeeT {
+
+	int id , salary;
+	String name,address;
+	public EmployeeT () {}
+	public EmployeeT(int empid, String empname ,String empaddress, int empsal){
+	   id = empid;
+	   name = empname;
+	   address = empaddress;
+	   salary = empsal;
+	}
+}
